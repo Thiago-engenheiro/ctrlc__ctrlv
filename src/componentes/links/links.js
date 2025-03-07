0bae3a1a0f52032db11d8f418f81991224b9c181
Thiago-engenheiro <thiago.principal11@outlook.com.br>
2025-03-07 16:38:09 -0300

    uptade

--- src/componentes/links/links.js
+++ src/componentes/links/links.js
@@ -2,16 +2,68 @@ import "./links.css";
 import "./linksResponsividade.css";
 import "./botaoFiltro.css";
 import "./card.css";
+import './subFiltro.css'
 
 import { useState } from "react";
 
 export default function Links() {
+
+  const [selecionadoGrupo1, setSelecionadoGrupo1] = useState("Todos");
+  const [selecionadoGrupo2, setSelecionadoGrupo2] = useState(null);
+  const [selecionadoGrupo3, setSelecionadoGrupo3] = useState(null);
+  const [selecionadoGrupo4, setSelecionadoGrupo4] = useState(null);
+  const [selecionadoGrupo5, setSelecionadoGrupo5] = useState(null);
   const [ativado, setAtividado] = useState(true);
-  const [ativadoBotao, setAtividadoBotao] = useState(false);
+  const [botoesAtivados, setBotoesAtivados] = useState({
+    codigo: false,
+    cores: false,
+    fontes: false,
+    sites: false,
+    pessoal: false,
+    imagens: false,
+
+  });
 
   const mudarEstadoDeAtivado = () => {
+    
     setAtividado(!ativado);
-    console.log("Ativado:", ativado);
+
+    setBotoesAtivados({
+      codigo: false,
+      cores: false,
+      fontes: false,
+      sites: false,
+      pessoal: false,
+      imagens: false,
+    });
+
+    setSelecionadoGrupo1("Nenhum"); 
+
+  };
+
+  const mudarEstadoDeAtivadoBotao = (botaoId) => {
+
+    setBotoesAtivados((prevState) => ({
+
+      ...prevState,
+      [botaoId]: !prevState[botaoId], 
+
+    }));
+
+  };
+
+  const handleCheckboxChange = (grupo, value) => {
+    if (grupo === "grupo1") {
+      setSelecionadoGrupo1(value); 
+    } else if (grupo === "grupo2") {
+      setSelecionadoGrupo2(value); 
+    } else if (grupo === "grupo3") {
+      setSelecionadoGrupo3(value); 
+    } else if (grupo === "grupo4") {
+      setSelecionadoGrupo4(value); 
+    } else if (grupo === "grupo5") {
+      setSelecionadoGrupo5(value); 
+    }
   };
 
   return (
@@ -30,7 +82,7 @@ export default function Links() {
             </div>
 
             <div className="continerFiltro">
-              <button disabled={ativado} className="botaoFiltro desativado">
+              <button disabled={ativado} onClick={() => mudarEstadoDeAtivadoBotao("codigo")}  className="botaoFiltro desativado">
                 <img
                   className="iconeImg"
                   src="/imagens/iconeTag/code-svgrepo-com.svg"
@@ -38,17 +90,62 @@ export default function Links() {
                 ></img>
                 Codigo
                 <img
-                  className="setaImg"
+                    className={`setaImg ${botoesAtivados.codigo ? 'rotated' : ''}`}
                   src="/imagens/iconeTag/seta.png"
                   alt="Icone"
                 ></img>
               </button>
 
-              <div className="continerSubFiltro"></div>
+              <div className="continerSubFiltro" hidden={!botoesAtivados["codigo"]}>
+
+              <div className="continerSelecaoFiltro nenhum">
+                  <input className="checkboxFiltro" type="checkbox" checked={selecionadoGrupo1 === "Nenhum"} 
+                  onChange={() => handleCheckboxChange("grupo1", "Nenhum")} ></input>
+                  <label className="textoFiltro">Nenhum</label>
+                </div>
+
+                <div className="continerSelecaoFiltro todos">
+                  <input className="checkboxFiltro" type="checkbox" checked={selecionadoGrupo1 === "Todos"} 
+                  onChange={() => handleCheckboxChange("grupo1", "Todos")} ></input>
+                  <label className="textoFiltro">Todos</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" checked={selecionadoGrupo1 === "Botoes"} 
+                  onChange={() => handleCheckboxChange("grupo1", "Botoes")} ></input>
+                  <label className="textoFiltro">Botoes</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" checked={selecionadoGrupo1 === "Cards"} 
+                  onChange={() => handleCheckboxChange("grupo1", "Cards")} ></input>
+                  <label className="textoFiltro">Cards</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" checked={selecionadoGrupo1 === "Inputs"} 
+                  onChange={() => handleCheckboxChange("grupo1", "Inputs")} ></input>
+                  <label className="textoFiltro">Inputs</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" checked={selecionadoGrupo1 === "Carregamento"} 
+                  onChange={() => handleCheckboxChange("grupo1", "Carregamento")} ></input>
+                  <label className="textoFiltro">Carregamento</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" checked={selecionadoGrupo1 === "Fundos"} 
+                  onChange={() => handleCheckboxChange("grupo1", "Fundos")} ></input>
+                  <label className="textoFiltro">Fundos</label>
+                </div>
+
+              </div>
+
             </div>
 
             <div className="continerFiltro">
-              <button disabled={ativado} className="botaoFiltro desativado">
+              <button disabled={ativado} onClick={() => mudarEstadoDeAtivadoBotao("cores")} className="botaoFiltro desativado">
                 <img
                   className="iconeImg"
                   src="/imagens/iconeTag/code-svgrepo-com.svg"
@@ -56,15 +153,45 @@ export default function Links() {
                 ></img>
                 Cores
                 <img
-                  className="setaImg"
+                  className={`setaImg ${botoesAtivados.cores ? 'rotated' : ''}`}
                   src="/imagens/iconeTag/seta.png"
                   alt="Icone"
                 ></img>
               </button>
+
+              <div className="continerSubFiltro" hidden={!botoesAtivados["cores"]}>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+              </div>
+
             </div>
 
             <div className="continerFiltro">
-              <button disabled={ativado} className="botaoFiltro desativado">
+              <button disabled={ativado}  onClick={() => mudarEstadoDeAtivadoBotao("fontes")} className="botaoFiltro desativado">
                 <img
                   className="iconeImg"
                   src="/imagens/iconeTag/code-svgrepo-com.svg"
@@ -72,15 +199,45 @@ export default function Links() {
                 ></img>
                 Fontes
                 <img
-                  className="setaImg"
+                  className={`setaImg ${botoesAtivados.fontes ? 'rotated' : ''}`}
                   src="/imagens/iconeTag/seta.png"
                   alt="Icone"
                 ></img>
               </button>
+
+              <div className="continerSubFiltro" hidden={!botoesAtivados["fontes"]}>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+              </div>
+
             </div>
 
             <div className="continerFiltro">
-              <button disabled={ativado} className="botaoFiltro desativado">
+              <button disabled={ativado} onClick={() => mudarEstadoDeAtivadoBotao("sites")} className="botaoFiltro desativado">
                 <img
                   className="iconeImg"
                   src="/imagens/iconeTag/code-svgrepo-com.svg"
@@ -88,15 +245,44 @@ export default function Links() {
                 ></img>
                 Sites
                 <img
-                  className="setaImg"
+                   className={`setaImg ${botoesAtivados.sites ? 'rotated' : ''}`}
                   src="/imagens/iconeTag/seta.png"
                   alt="Icone"
                 ></img>
               </button>
+
+              <div className="continerSubFiltro" hidden={!botoesAtivados["sites"]}>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+              </div>
             </div>
 
             <div className="continerFiltro">
-              <button disabled={ativado} className="botaoFiltro desativado">
+              <button disabled={ativado} onClick={() => mudarEstadoDeAtivadoBotao("pessoal")} className="botaoFiltro desativado">
                 <img
                   className="iconeImg"
                   src="/imagens/iconeTag/code-svgrepo-com.svg"
@@ -104,15 +290,44 @@ export default function Links() {
                 ></img>
                 Pessoal
                 <img
-                  className="setaImg"
+                  className={`setaImg ${botoesAtivados.pessoal ? 'rotated' : ''}`}
                   src="/imagens/iconeTag/seta.png"
                   alt="Icone"
                 ></img>
               </button>
+
+              <div className="continerSubFiltro" hidden={!botoesAtivados["pessoal"]}>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+              </div>
             </div>
 
             <div className="continerFiltro">
-              <button disabled={ativado} className="botaoFiltro desativado">
+              <button disabled={ativado} onClick={() => mudarEstadoDeAtivadoBotao("imagens")} className="botaoFiltro desativado">
                 <img
                   className="iconeImg"
                   src="/imagens/iconeTag/code-svgrepo-com.svg"
@@ -120,11 +335,40 @@ export default function Links() {
                 ></img>
                 Imagens
                 <img
-                  className="setaImg"
+                  className={`setaImg ${botoesAtivados.imagens ? 'rotated' : ''}`}
                   src="/imagens/iconeTag/seta.png"
                   alt="Icone"
                 ></img>
               </button>
+
+              <div className="continerSubFiltro" hidden={!botoesAtivados["imagens"]}>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+                <div className="continerSelecaoFiltro">
+                  <input className="checkboxFiltro" type="checkbox" ></input>
+                  <label className="textoFiltro">Nome aqui</label>
+                </div>
+
+              </div>
             </div>
           </div>
         </div>
@@ -133,7 +377,19 @@ export default function Links() {
 
         <div className="continerCardsLinks">
           <div className="card">
-            <div class="img"></div>
+            <div class="img">
+
+            <video 
+            className="setaAnimada" 
+            src="/imagens/imagensParaLinks/codigo/botoes/botaoComSetaAnimado.mp4" 
+            type="video/mp4" 
+            autoPlay 
+            loop 
+            muted 
+            playsInline
+          ></video>
+
+            </div>
             <div class="continerIconeImg codigo">
               <img
                 className="iconeImgCard"
@@ -141,17 +397,14 @@ export default function Links() {
                 alt="Icone"
               ></img>
             </div>
-            <span>Nome do card</span>
+            <span>Botão seta animado</span>
 
             <p class="info">
-              Vai ter um texto legal aqui sobre, Vai ter um texto legal aqui
-              sobre , Vai ter um texto legal aqui sobre , Vai ter um texto legal
-              aqui sobre , Vai ter um texto legal aqui sobre, Vai ter um texto
-              legal aqui sobre
+              Botão com seta animada ao passa o mause.
             </p>
 
-            <a className="linkCard" href="a">
-              Link para o codigo
+            <a className="linkCard" href="https://uiverse.io/adamgiebl/smart-moth-68">
+              Link para o codigo(a)
             </a>
           </div>
 
@@ -174,7 +427,7 @@ export default function Links() {
             </p>
 
             <a className="linkCard" href="a">
-              Link para o codigo
+              Link para a cor(a)
             </a>
           </div>
         </div>
